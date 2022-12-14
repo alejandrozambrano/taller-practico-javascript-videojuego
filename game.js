552cc36522e27255acb7df56d3139010c6ff1354
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-12-14 11:29:42 -0500

    Objeto positionPlayer

--- game.js
+++ game.js
@@ -8,6 +8,11 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+}
+
 window.addEventListener('load', setCanvaSize);
 window.addEventListener('resize', setCanvaSize);
 
@@ -42,10 +47,25 @@ function startGame() {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1);
             const posY = elementsSize * (rowI + 1);
+
+            if (col == 'O') {
+              playerPosition.x = posX;
+              playerPosition.y = posY;
+              console.log({playerPosition});
+            }
+
             game.fillText(emoji, posX, posY);
         });
     });
+  
+    movePlayer();
 }
+
+function movePlayer() {
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y); 
+}
+
+
 window.addEventListener('keydown', moveByKeys);
 btnUp.addEventListener('click', moveUp);
 btnLeft.addEventListener('click', moveLeft);
@@ -60,6 +80,8 @@ function moveByKeys(event) {
 }
 function moveUp() {
   console.log('Me quiero mover hacia arriba');
+  playerPosition.y -= elementsSize;
+  movePlayer();
 }
 function moveLeft() {
   console.log('Me quiero mover hacia izquierda');
